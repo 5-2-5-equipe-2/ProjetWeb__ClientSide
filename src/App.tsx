b2c82f0bf8f26861d69a8a6c326d3f4311542fe2
Apoorva64 <appadooapoorva@gmail.com>
2022-04-26 12:31:04 +0200

    - React project init - installed   - react query   - react router   - react bootstrap   - gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/images/logo.svg';
+import './media/css/App.css';
 
 function App() {
   return (
