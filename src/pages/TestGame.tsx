3eb9f3e71c393bb0940db732f329fa79f35e7b70
Gradou06 <gradou06@gmail.com>
2022-06-02 01:06:10 +0200

    morpion ending

--- src/pages/TestGame.tsx
+++ src/pages/TestGame.tsx
@@ -5,6 +5,7 @@ import "../media/css/TestGame.css";
 
 let squares = Array(9).fill(null);
 let xIsNext = true;
+let isWin = false;
   
 const Row = (props : any) => {
     return <div className="board-row">{React.Children.map(props.children, (child) => child)}</div>;
@@ -18,12 +19,48 @@ const Square = (props : any) => {
     );
 }
 
+function getRandomInt(min: any, max: any) : number{
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
+function checkWin() {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            isWin = true;
+            return;
+        }
+    }
+}
+
+function iaPlay() {
+    let i = getRandomInt(0, 8);
+    while(squares[i] !== null) {
+        i = getRandomInt(0, 8);
+    }
+    squares[i] = "O";
+    renderSquare(squares[i]);
+    checkWin();
+}
 
 function onClick(i :number){
-    if(squares[i] === null) {
-        squares[i] = xIsNext ? "X" : "O";
-        xIsNext = !xIsNext;
+    if(squares[i] === null && !isWin) {
+        squares[i] = "X";
         renderSquare(squares[i]);
+        checkWin();
+        iaPlay();
     } 
 }
 
