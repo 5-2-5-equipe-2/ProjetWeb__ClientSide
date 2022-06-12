e25d2d8677c25150dcce232e4f97db8656ce850e
Apoorva64 <appadooapoorva@gmail.com>
2022-06-12 23:18:39 +0200

    refactor: changed to alwaydata server

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true
-const BASE_URL = 'http://localhost/api.php/'
+const BASE_URL = 'https://cinqplusdeuxegalcinq.alwaysdata.net/api.php/'
 
 const api = axios.create({
     baseURL: BASE_URL,
